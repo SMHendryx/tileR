04bf3c7c8d3ad20da8de90116475cf6debbb5207
Sean Hendryx <seanmhendryx@gmail.com>
2017-03-31 10:54:09 -0700

    segmentation working but writeLAS error persists

--- tileR.R
+++ tileR.R
@@ -37,17 +37,17 @@ tileR <- function(las, catalogDimensions){
   
   #catalogDimenions should be 2D:
   if(length(catalogDimensions) != 2){
-    stop("catalogDimenions must equal 2")
+    stop("catalogDimenions must be vector of length 2")
   }
   
   #extent:
   minX = min(las@data[,X])
   maxX = max(las@data[,X])
-  minY = min(las@data[,Y])
-  maxY = max(las@data[,Y])
+  #minY = min(las@data[,Y])
+  #maxY = max(las@data[,Y])
   
   #Compute height and width:
-  h = maxY - minY
+  #h = maxY - minY
   w = maxX - minX
 
 
@@ -55,24 +55,49 @@ tileR <- function(las, catalogDimensions){
   numRows = catalogDimensions[1]
   numColumns = catalogDimensions[2] 
   #compute column (stepX) and row widths:
-  stepY = h/numRows
+  #stepY = h/numRows
+  #print(paste0("stepY ", stepY, "\n"))
   stepX = w/numColumns
+  print(paste0("stepX ", stepX, "\n"))
   
-  minY_i = minY
-  minX_i = minX
+  #minY_i = minY
+  minX_column = minX
   tileNum = 1
-  
+
   for(i_column in seq(numColumns)){
     #compute the borders of each column:
-    maxX_i = minX_i + stepX
+    maxX_column = minX_column + stepX
+    print(paste0("maxX_column", maxX_column, "\n"))
+
     #make the column of points:
-    col_i_points = LAS(las@data[X >= minX_i & X <= maxX_i])
+    col_i_points = LAS(las@data[X >= minX_column & X <= maxX_column])
+    plot(col_i_points)
+    # Find the minimum Y coordinate of the column of points:
+    minY = min(col_i_points@data[,Y])
+    #instantiate the minimum of the tile:
+    minY_tile = minY
+    maxY = max(col_i_points@data[,Y])
+    #stop("column should be plotted")
+    #height of column:
+    h = maxY - minY
+    #height of each tile in the column:
+    stepY = h/numRows
     #then from the column pull the row to make the tile:
     for(i_row in seq(numRows)){
-      maxY_i = minY_i + stepY
-      tile_i_points = LAS(col_i_points@data[Y >= minY_i & Y <= maxY_i])
+      print("Inside inner for loop (making tiles from each column).")
+      maxY_tile = minY_tile + stepY
+      print(paste0("maxY_tile", maxY_tile, "\n"))
+      tile_i_points = LAS(col_i_points@data[Y >= minY_tile & Y <= maxY_tile])
+      plot(tile_i_points)
       writeLAS(tile_i_points, paste0("tile-", tileNum, ".las"))
+      #stop("should have written tile-1.las")
+      
+      #increment minY_i
+      minY_tile = maxY_tile 
       tileNum = tileNum + 1
     }
+
+    #increment minX_column:
+    minX_column = maxX_column
   }
 }
--- tileR_tests.R
+++ tileR_tests.R
@@ -3,9 +3,9 @@
 rm(list=ls())
 library(lidR)
 
-setwd("/Users/seanhendryx/DATA/Lidar/SRER/maxLeafAreaOctober2015/inputData/outputDirectory")
+setwd("/Users/seanhendryx/DATA/Lidar/SRER/maxLeafAreaOctober2015/inputData/outputDirectory/tiles")
 
-tLidar = readLAS("Decimated_Cropped_SOR_Cleaned_CC-Default-tLidarSRERMesTowerOct2015.las")
+tLidar = readLAS("../Rectangular_Decimated_SOR_Cleaned_CC-Default-tLidarSRERMesTowerOct2015.las")
 
 source("/Users/seanhendryx/githublocal/tileR/tileR.R")
 
